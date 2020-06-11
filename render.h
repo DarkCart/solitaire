5de9e6d961a29deddcc2d0e2fb2eb322addb7037
John <minecart94@gmail.com>
2020-06-10 23:22:50 -0500

    remove duplicated render code

--- render.h
+++
@@ -1,6 +0,0 @@
-void initRenderer(SDL_Renderer* r);
-void render();
-void renderCard(struct Card card, int x, int y);
-void renderCardStack(struct Card stack[52], int originalSize, int beginX, int beginY, int spacing);
-void renderPlayingField();
-void destroyRenderer();
